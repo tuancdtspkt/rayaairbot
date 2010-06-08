29086beb7ece97a1ae5990c7163d8d38c9105401
lcasassa <lcasassa@47859eb8-05d1-d979-46d7-8c395c050647>
2010-06-08 13:36:05 +0000

    Control.c usa Pid.c

--- trunk/microcontroller/Quadrotor/Control.c
+++ trunk/microcontroller/Quadrotor/Control.c
@@ -6,6 +6,7 @@
 #include "Gyros.h"
 #include "Accelerometer.h"
 #include "Hardware.h"
+#include "Pid.h"
 
 // gADC[0] -> gyro x
 // gADC[1] -> gyro y
@@ -21,24 +22,15 @@
 // motor[2] -> Este
 // motor[3] -> Norte
 
-int8_t P[2]={0,0};
-int8_t I[2]={0,0};
-int8_t D[2]={0,0};
+struct PID pid[2];
 
 int8_t emergenciaSTOP=1;
-
-int16_t I_Max[2]={0,0};
-int16_t I_Min[2]={0,0};
 
 int16_t u[4] = {0,0,0,0};
 int16_t omega=0; 
 
 void Control(void) // Timer.c llama esta funcion cada 1ms
 {
-    // variables PID
-    static int16_t integrador[4];
-    static int16_t e_old[4];
-    int16_t e[4];
     // variables auxiliares
     uint8_t i;
     static uint8_t k=0;
@@ -52,50 +44,25 @@ void Control(void) // Timer.c llama esta funcion cada 1ms
     GetGyros();
     GetAccelerometer();
 
-    //cada 200ms ejecuta esto:
+    //cada 50ms ejecuta esto:
     if(periodo%50 == 1) {
         periodo=1;
-        i=1;
-        e[i] = joystick[0]/10 - angle[0];
+        omega = updatePID(&pid[0], joystick[0], angle[0]);
 
-        integrador[i] += I[i]*e[i];
-
-        // Antienrrollamiento
-        if(integrador[i] > I_Max[i])
-            integrador[i] = I_Max[i];
-        else if(integrador[i] < I_Min[i])
-            integrador[i] = I_Min[i];
-
-        omega = P[i]*e[i]/4 + integrador[i]/1000 + D[i]*(e[i] - e_old[i])/10;
-
-        e_old[i] = e[i];
         if(k++%128==0) {
-            printf("A: e: %d P: %d I:%d D:%d II:%d\n", e[1], P[1], I[1], D[1], integrador[1]);
-            printf("G: e: %d P: %d I:%d D:%d II:%d\n", e[0], P[0], I[0], D[0], integrador[0]);
+            printf("A: P: %d I:%d D:%d II:%d\n", pid[0].p, pid[0].I, pid[0].D, pid[0].integratedError);
+            printf("G: P: %d I:%d D:%d II:%d\n", pid[1].p, pid[1].I, pid[1].D, pid[1].integratedError);
         }
         
     }
 
 
     // PID
-    for(i=0; i<1; i++) {
-        e[i] = gyro[i] - omega;
+    u[0] = updatePID(&pid[0], omega, gyro[0]);
 
-        integrador[i] += I[i]*e[i];
-
-        // Antienrrollamiento
-        if(integrador[i] > I_Max[i])
-            integrador[i] = I_Max[i];
-        else if(integrador[i] < I_Min[i])
-            integrador[i] = I_Min[i];
-
-        u[i] = P[i]*e[i]/10 + integrador[i]/1000 + D[i]*(e[i] - e_old[i])/10;
-
-        e_old[i] = e[i];
-    }
 
 //    u[0] = joystick[0];
-//    u[1] = joystick[1];
+    u[1] = joystick[1];
     u[2] = joystick[2];
     u[3] = joystick[3];
 
