714f63303c1d5363c99c33a6189bf7ae89ab21da
lcasassa <lcasassa@47859eb8-05d1-d979-46d7-8c395c050647>
2010-06-19 13:37:22 +0000

    TCP envia y recibe comandos

--- trunk/gumstix/files/main.cpp
+++ trunk/gumstix/files/main.cpp
@@ -1,12 +1,15 @@
 #include <QCoreApplication>
 #include <QDebug>
 
+#include "rayaairbot.h"
 
 int main(int argc, char** argv)
 {
 	QCoreApplication app(argc, argv);
 
-	qDebug() << "Hello world";
+        RayaAirBot rayaairbot;
+
+        qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
 
 	return app.exec();
 }
