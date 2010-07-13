5e5b699ceceec5e63b849f9eb3f4a9059102b59a
lcasassa <lcasassa@47859eb8-05d1-d979-46d7-8c395c050647>
2010-07-13 18:58:17 +0000

    Actualizando Factores

--- microcontroller/Quadrotor/Gyros.h
+++ microcontroller/Quadrotor/Gyros.h
@@ -6,8 +6,11 @@ extern int16_t gyro[2];
 void InitGyros(void);
 void GetGyros(void);
 
-// Multiply by this factor tu convert gyro[] in rad/s units.
+// Multiply by this factor to convert gyro[] in rad/s units.
 #define CONVERT_TO_RAD_S 0.0131030725 
+
+// Multiply by this factor to convert gyro[] in degree/s units.
+#define CONVERT_TO_DEGREE_S 0.750750753
 
 
 #endif
