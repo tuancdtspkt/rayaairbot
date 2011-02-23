3209e2c4100e01f605913d2dc44c2ff84ae13aa7
lcasassa <lcasassa@47859eb8-05d1-d979-46d7-8c395c050647>
2011-02-23 19:53:34 +0000

    New version on libopenstm32

---
+++ trunk/microcontroller/stm32/nvic_.c
@@ -0,0 +1,23 @@
+#include "nvic_.h"
+
+void nvic_setup()
+{
+	nvic_enable_irq(NVIC_HARD_FAULT_IRQ);
+	nvic_set_priority(NVIC_HARD_FAULT_IRQ, 0);
+
+	nvic_enable_irq(NVIC_EXTI0_IRQ);
+	nvic_set_priority(NVIC_EXTI0_IRQ, 1);
+
+	nvic_enable_irq(NVIC_USART1_IRQ);
+	nvic_set_priority(NVIC_USART1_IRQ, 2);
+
+	nvic_enable_irq(NVIC_DMA1_CHANNEL1_IRQ);
+	nvic_set_priority(NVIC_DMA1_CHANNEL1_IRQ, 3);
+
+	nvic_enable_irq(NVIC_TIM2_IRQ);
+	nvic_set_priority(NVIC_TIM2_IRQ, 4);
+
+	nvic_enable_irq(NVIC_TIM3_IRQ);
+	nvic_set_priority(NVIC_TIM3_IRQ, 5); //1
+
+}
